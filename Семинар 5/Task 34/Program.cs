8e79a753ffa23db76d34e06af9b25b3b462a5604
ivanovalexey1993 <ivanovalexey1993@mail.ru>
2022-09-06 21:27:35 +0300

    Task 36

--- Семинар 5/Task 34/Program.cs
+++ Семинар 5/Task 34/Program.cs
@@ -1,21 +1,29 @@
-﻿int[] array = randomArray(4);
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+
+int[] array = randomArray(4);
 Console.WriteLine($"массив:[{string.Join(",",array)}] -> Количество чётных чисел в массиве: {countOfEven(array)}");
 
 int[] randomArray(int size) 
 {
-    Random random = new Random();
+    Random R = new Random();
     int[] array = new int[size];
     for(int i = 0; i < array.Length; i++) 
     {
-        array[i] = random.Next(100,1000);
+        array[i] = R.Next(100,1000);
     }
     return array;
 }
 
-int countOfEven(int[] array) {
+int countOfEven(int[] array) 
+{
     int count = 0;
-    for(int i = 0; i < array.Length; i++) {
-        if(array[i] % 2 == 0) {
+    for(int i = 0; i < array.Length; i++) 
+    {
+        if(array[i] % 2 == 0) 
+        {
             count++;
         }
     }
---
+++ Семинар 5/Task 36/Program.cs
@@ -0,0 +1,27 @@
+﻿int[] array = randomArray(4);
+Console.WriteLine($"массив:[{string.Join(",",array)}] -> сумма элементов, стоящих на нечётных позициях = {sumOfOdd(array)}");
+
+int[] randomArray(int size) 
+{
+    Random R = new Random();
+    int[] array = new int[size];
+    for(int i = 0; i < array.Length; i++) 
+    {
+        array[i] = R.Next(-10,100);
+    }
+    return array;
+}
+
+int sumOfOdd(int[] array) 
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++) 
+    {
+        if(i % 2 != 0) 
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
