209b752329df07d5e9d1104ad823f2e44aa97b79
ivanovalexey1993 <ivanovalexey1993@mail.ru>
2022-09-17 21:23:36 +0300

    Task 58

---
+++ Семинар 8/Task 58/Program.cs
@@ -0,0 +1,56 @@
+﻿int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 5);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+
+int size = InputInt("размер матриц: ");
+int[,] matrixX = new int[size, size];
+int[,] matrixY = new int[size, size];
+FillArrayRandomNumbers(matrixX);
+FillArrayRandomNumbers(matrixY);
+int[,] matrixZ = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        for (int k = 0; k < size; k++)
+        {
+            matrixZ[i, j] = matrixZ[i, j] + (matrixX[i, k] * matrixY[k, j]);
+        }
+    }
+}
+Console.WriteLine("Матрица - X");
+PrintArray(matrixX);
+Console.WriteLine();
+Console.WriteLine("Матрица - Y");
+PrintArray(matrixY);
+Console.WriteLine();
+Console.WriteLine("X*Y");
+PrintArray(matrixZ);
