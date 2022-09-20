df52c35ee13d6b4b7f3c7a3944462f9ac6f45991
ivanovalexey1993 <ivanovalexey1993@mail.ru>
2022-09-20 20:03:38 +0300

    Task 66

---
+++ Семинар 9/Task 66/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите N:");
+int numberN = int.Parse(Console.ReadLine());
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
